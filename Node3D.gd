9d2930eaf57bc4669df59c041d72076bb6f90691
maxlucaschwab <114507212+maxlucaschwab@users.noreply.github.com>
2023-10-15 13:02:44 -0700

    obstacles

--- Node3D.gd
+++ Node3D.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 var transition = false
-var openAnimation = false
+#var openAnimation = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,9 +14,9 @@ func _process(delta):
 	#if Input.is_mouse_button_pressed(1):
 	#	$cameraAnim.play("fishing_transition")
 	mainGameplay()
-	if transition == true and openAnimation == true:
-		$Camera3D.position += Vector3(0, -0.03, 0)
-		$hook.position += Vector3(0, -0.02, 0)
+	if transition == true: #and openAnimation == true:
+		$Camera3D.position += Vector3(0, -0.01, 0)
+		$hook.position += Vector3(0, -0.008, 0)
 	pass
 	
 	if Input.is_action_just_pressed("pause"):
@@ -25,18 +25,18 @@ func _process(delta):
 		get_tree().paused = true
 		#get_tree().quit()
 	
-	if $hook.index == 3:
+	if $hook.index == 3: #win condition, fix later
 		transition = false
 		$Camera3D.position = $empty_0.position
 		$Camera3D.rotation = $empty_0.rotation
 
 
 func mainGameplay():
-	if Input.is_action_just_pressed("cast") and transition == false and openAnimation == false:
+	if Input.is_action_just_pressed("cast") and transition == false: #and openAnimation == false:
 		$Camera3D/cameraAnim.play("fishing_transition")
 		$hook/hookAnim.play("hook_throw")
 		transition = true
-		openAnimation = true
+		#openAnimation = true
 		_on_camera_anim_animation_finished("fishing_transition")
 	pass
 
--- hook.gd
+++ hook.gd
@@ -5,9 +5,11 @@ var entered = false
 var index = 0
 var constellation = {0:[-23, 19, -17], 1:[-8, 11, -26], 2:[8, 21, -26]}
 
+var starPos 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$hookAnim.play("hook_bob")
+	#$hookAnim.play("hook_bob")
 	pass # Replace with function body.
 
 
@@ -25,5 +27,18 @@ func _on_area_entered(area):
 	print("hello")
 	if area.has_method("hooked"):
 		
-		area.position = Vector3(constellation[index][0], constellation[index][1], constellation[index][2])
+		$"..".transition = false
+		#$"..".openAnimation = false
+		
+		create_tween().tween_property(self, "position", $"../empty_1".position, 2)
+		
+		create_tween().tween_property($"../Camera3D", "position", $"../empty_0".position, 2)
+		create_tween().tween_property($"../Camera3D", "rotation", $"../empty_0".rotation, 2)
+		
+		create_tween().tween_property(area, "position", $"../empty_1".position, 2)
+		
+		await get_tree().create_timer(2).timeout
+		#$hookAnim.play("hook_bob")
+		starPos = Vector3(constellation[index][0], constellation[index][1], constellation[index][2])
+		create_tween().tween_property(area, "position", starPos, 2)
 		index += 1
