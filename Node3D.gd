c1d452d8e918ea8026f5310fb41716136d1f015f
maxlucaschwab <114507212+maxlucaschwab@users.noreply.github.com>
2023-10-15 17:27:41 -0700

    gaem done

--- Node3D.gd
+++ Node3D.gd
@@ -1,11 +1,13 @@
 extends Node3D
 
 var transition = false
-#var openAnimation = false
+var winCondition = false
+var openAnimation = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	winCondition == false
 	pass
 	
 
@@ -16,25 +18,38 @@ func _process(delta):
 	mainGameplay()
 	if transition == true: #and openAnimation == true:
 		$Camera3D.position += Vector3(0, -0.01, 0)
-		$hook.position += Vector3(0, -0.008, 0)
+		$hook.position += Vector3(0, -0.005, 0)
 	pass
 	
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_action_just_pressed("pause") and winCondition == false:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		$"../Control".show()
+		$"../menu".show()
 		get_tree().paused = true
 		#get_tree().quit()
 	
-	if $hook.index == 3: #win condition, fix later
+	if $hook.index == 4: #win condition, fix later
+		openAnimation = true
 		transition = false
-		$Camera3D.position = $empty_0.position
-		$Camera3D.rotation = $empty_0.rotation
+		winCondition = true
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		$"../menu".show()
+		$"../menu/Resume".hide()
+		$"../menu/Label".hide()
+		$"../tutorial/direction".text = "You Win!"
+		
 
 
 func mainGameplay():
-	if Input.is_action_just_pressed("cast") and transition == false: #and openAnimation == false:
+	if Input.is_action_just_pressed("cast") and transition == false and openAnimation == false:
+		
 		$Camera3D/cameraAnim.play("fishing_transition")
 		$hook/hookAnim.play("hook_throw")
+		
+		$"../tutorial/caption".hide()
+		$"../tutorial/direction".text = "use MOUSE to move!"
+		await get_tree().create_timer(2).timeout
+		$"../tutorial/direction".hide()
+		
 		transition = true
 		#openAnimation = true
 		_on_camera_anim_animation_finished("fishing_transition")
--- hook.gd
+++ hook.gd
@@ -3,23 +3,29 @@ extends Area3D
 var entered = false
 
 var index = 0
-var constellation = {0:[-23, 19, -17], 1:[-8, 11, -26], 2:[8, 21, -26]}
+var constellation = {0:[-23, 19, -17], 1:[-0.5, 16, -30], 2:[5, 12, -36], 3:[7, 10, -43]}
 
 var starPos 
 
+var tutorialText = "PRESS LMB TO CAST"
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.position = $"../empty_1".position
 	#$hookAnim.play("hook_bob")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var boatNode = get_node("../boat")
+	#var hookPos = boatNode.position + $"../empty_1".position
+	if $"..".transition == false:
+		self.position = boatNode.position + $"../empty_1".position
 	pass
 
 func _input(event):
 	if event is InputEventMouseMotion and $"..".transition == true:
-		translate_object_local(Vector3(event.relative.x/5, -event.relative.y/5, 0).normalized())
+		translate_object_local(Vector3(event.relative.x/10, -event.relative.y/10, 0).normalized())
 
 
 
@@ -27,13 +33,13 @@ func _on_area_entered(area):
 	print("hello")
 	if area.has_method("hooked"):
 		
-		$"..".transition = false
 		#$"..".openAnimation = false
-		
+		#.disabled = true
 		create_tween().tween_property(self, "position", $"../empty_1".position, 2)
 		
 		create_tween().tween_property($"../Camera3D", "position", $"../empty_0".position, 2)
 		create_tween().tween_property($"../Camera3D", "rotation", $"../empty_0".rotation, 2)
+		create_tween().tween_property($"../Camera3D", "fov", 75, 2)
 		
 		create_tween().tween_property(area, "position", $"../empty_1".position, 2)
 		
@@ -41,4 +47,21 @@ func _on_area_entered(area):
 		#$hookAnim.play("hook_bob")
 		starPos = Vector3(constellation[index][0], constellation[index][1], constellation[index][2])
 		create_tween().tween_property(area, "position", starPos, 2)
+		
 		index += 1
+		
+		$"..".transition = false
+		
+		if $"..".winCondition == false:
+			$"../../tutorial/direction".text = tutorialText
+			$"../../tutorial/direction".show()
+	
+	if area.has_method("wall"):
+		create_tween().tween_property(self, "position", $"../empty_1".position, 2)
+		
+		create_tween().tween_property($"../Camera3D", "position", $"../empty_0".position, 2)
+		create_tween().tween_property($"../Camera3D", "rotation", $"../empty_0".rotation, 2)
+		create_tween().tween_property($"../Camera3D", "fov", 75, 2)
+		
+		await get_tree().create_timer(2).timeout
+		$"..".transition = false
